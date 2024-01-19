7c74284dd22ac51eb8421b0f5b6a1a9e85fdb66b
younessab12 <younesseaabibi828@gmail.com>
2024-01-19 13:31:18 +0100

    bruh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,14 +37,14 @@ export default function HomePage() {
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white pb-6">
-      <h1 className="text-4xl font-bold m-6">Admin Dashboard</h1>
+    <main className="text-center flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white pb-6">
+      <h1 className="text-4xl font-bold mt-10">Admin Dashboard</h1>
       {/* {typeof(lastRefresh) == String ? lastRefresh : lastRefresh.toISOString()} */}
       {lastRefresh}
-
+      <div className='mb-8'></div>
       <div className='flex gap-24 '>
 
-      <div className='mt-5 px-6 py-4 bg-gray-100 w-[16rem] text-black rounded-2xl shadow-xl h-full'>
+      <div className='text-left mt-5 px-6 py-4 bg-gray-100 w-[16rem] text-black rounded-2xl shadow-xl h-full'>
         <h3 className='flex justify-center items-center pt-2 pb-2'>Operators Live</h3>
         {
           operatorsStatus.map(
@@ -68,7 +68,7 @@ export default function HomePage() {
         }
       </div>
 
-      <table className="px-10 py-4 whitespace-nowrap text-sm text-yellow mt-5 border-2 rounded-xl bg-gray-100 text-black">
+      <table className="overflow-hidden px-10 py-4 whitespace-nowrap text-sm text-yellow mt-5 rounded-3xl bg-gray-100 text-black">
         <td className="px-6 py-4 whitespace-nowrap text-sm font-bold flex justify-center">
           Time
         </td>
